6950fb85348ffb767509d7280d58596911056831
RunningTHPed <pednoiiz008@gmail.com>
2022-05-18 03:28:31 +0700

    harris corner detection

---
+++ Harris Corner Detection-Python/Harris_Corner_Detection.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+
+def harris_corner_detection(input_img, k, window_size, threshold):
+    
+    corner_list = []
+    output_img = cv2.cvtColor(input_img.copy(), cv2.COLOR_GRAY2RGB)
+    
+    offset = int(window_size/2)
+    y_range = input_img.shape[0] - offset
+    x_range = input_img.shape[1] - offset
+    
+    
+    dy, dx = np.gradient(input_img)
+    Ixx = dx**2
+    Ixy = dy*dx
+    Iyy = dy**2
+    
+    
+    for y in range(offset, y_range):
+        for x in range(offset, x_range):
+            
+            #Values of sliding window
+            start_y = y - offset
+            end_y = y + offset + 1
+            start_x = x - offset
+            end_x = x + offset + 1
+            
+            #The variable names are representative to the variable of the Harris corner equation
+            windowIxx = Ixx[start_y : end_y, start_x : end_x]
+            windowIxy = Ixy[start_y : end_y, start_x : end_x]
+            windowIyy = Iyy[start_y : end_y, start_x : end_x]
+            
+            #Sum of squares of intensities of partial derevatives 
+            Sxx = windowIxx.sum()
+            Sxy = windowIxy.sum()
+            Syy = windowIyy.sum()
+
+            #Calculate determinant and trace of the matrix
+            det = (Sxx * Syy) - (Sxy**2)
+            trace = Sxx + Syy
+            
+            #Calculate r for Harris Corner equation
+            r = det - k*(trace**2)
+
+            if r > threshold:
+                corner_list.append([x, y, r])
+                output_img[y,x] = (0,0,255)
+    
+    return output_img 
+
+
+def main():
+    k = 0.04
+    window_size = 3
+    threshold = 10000.00
+        
+    input_img = cv2.imread('chessboard.png', 0)
+    
+    cv2.imshow('Input Image', input_img)
+    
+    if input_img is not None:
+        corner_img = harris_corner_detection(input_img, k, window_size, threshold)
+        cv2.imshow('Detected Corners', corner_img)
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()
